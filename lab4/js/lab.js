ca4c177bcb80970bc3b7080df2e818d892539bbb
Magmyte <charleshaiwen@gmail.com>
2025-04-17 22:26:02 -0700

    Lab 4 Assignment

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Minesweeper
+// An implementation of the game
+// Kimiko Yamaguchi & Charles Haiwen
+//17 April 2024
 
-// Constants
+// Pseudocode for Minesweeper:
 
-// Functions
+// User chooses the size of the map
+// Program generates objects on the map
+//     Choose number of mines based on map size
+//     Select random positions of tiles on map
+//     Generate numbers for tiles based on mine positions
+// Display UI to the player
+//     Obscure tiles
+//     Include counter for mines left on the map
+//     Include timer
+// Check for mouse click on a tile
+//     If left click: reveal the tile
+//         If the tile is a mine: Game Over
+//         Else: display the tile
+//         If the tile has no number, display all touching tiles with no number
+//     If right click: place a marker on the tile
+//     If all non-mine tiles are revealed: Game Win
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
