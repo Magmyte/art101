8e36450638729087940ea9e17085c59bd0a9e6d1
Magmyte <charleshaiwen@gmail.com>
2025-04-26 21:36:52 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 6 Assignment
+// Charles Haiwen & 
+// 26 April 2025
 
-// Constants
+// initiatize variables
+var myTransport = ["Nissan skyline", "Santa Cruz Metro"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//create object
+myMainRide = {
+  make: "Nissan",
+  model: "Skyline",
+  color: "red",
+  year: 1995,
+  age: function() {
+    return 2025 - year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Kinds of transportation I use: " + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
