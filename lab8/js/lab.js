1ba0530b2e84244408c679c62175f835dbf589c0
Magmyte <charleshaiwen@gmail.com>
2025-05-01 14:05:26 -0700

    Lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,21 +1,30 @@
-// Lab 6 Assignment
+// Lab 8 Assignment
 // Charles Haiwen & 
-// 26 April 2025
+// 30 April 2025
 
 // initiatize variables
-var myTransport = ["Nissan skyline", "Santa Cruz Metro"];
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("The basic array: ", arr);
 
-//create object
-myMainRide = {
-  make: "Nissan",
-  model: "Skyline",
-  color: "red",
-  year: 1995,
-  age: function() {
-    return 2025 - year;
-  }
+// define square function - returns the input squared
+function sqr(inputVar) {
+  return inputVar * inputVar;
 }
 
-//output
-document.writeln("Kinds of transportation I use: " + myTransport + "<br>");
-document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
+// test the squaring function
+var result1 = arr.map(sqr);
+
+// should return an array: (10) [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+console.log("The array with square numbers: ", result1);
+
+// test the addition function, adding one to each element of the array
+var result2 = arr.map(function(inputVar) {
+  inputVar++;
+  return inputVar;
+})
+
+// should return an array: (10) [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+console.log("The array with every number added to 1: ", result2);
+
+// use jQuery to select the element by its ID and set the HTML content
+$("#output").html("The basic array: ", arr, "<br>The array with square numbers: ", result1, "<br>The array with every number added to 1: ", result2, "</br>");
