c8e0ab0eca61a48c5278efafddf495bb1ceda2f7
Magmyte <charleshaiwen@gmail.com>
2025-05-12 20:47:15 -0700

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,6 +4,7 @@
    Requirements: jQuery must be loaded for this script to work.
 
    Lab 9 Assignment
+<<<<<<< Updated upstream
    Author: Charles Haiwen & Austin Allen
    Date: 10 May 2025
 */
@@ -15,4 +16,17 @@ $(".button-press").append("<button class='button-challenge'>Make Special</button
 $(".button-challenge").click(function(){
   // now add (or subtract) the "special" class to the section
   $(this).parent().toggleClass("special");
+=======
+   Author: Charles Haiwen
+   Date: 5 May 2025
+*/
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#challenge").toggleClass("special");
+>>>>>>> Stashed changes
 });
