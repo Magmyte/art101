d703d6c66997b4c75bafc187ed32cf95954a82bf
Magmyte <charleshaiwen@gmail.com>
2025-05-15 12:08:10 -0700

    Lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,21 +1,28 @@
-// Lab 10 Assignment
-// Charles Haiwen & 
-// 26 April 2025
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
 
-// initiatize variables
-var myTransport = ["Nissan skyline", "Santa Cruz Metro"];
+   Requirements: jQuery must be loaded for this script to work.
+Lab 10 Assignment
+Authors: Charles Haiwen & Austin Allen
+Date: 15 May 2025
+*/
 
-//create object
-myMainRide = {
-  make: "Nissan",
-  model: "Skyline",
-  color: "red",
-  year: 1995,
-  age: function() {
-    return 2025 - year;
-  }
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
 }
 
-//output
-document.writeln("Kinds of transportation I use: " + myTransport + "<br>");
-document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomText();
+  
+  // append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
