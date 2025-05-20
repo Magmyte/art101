dbe46404c94750ee325d77d06eddaf5e91c2fdf0
Magmyte <charleshaiwen@gmail.com>
2025-05-19 17:26:13 -0700

    Lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,39 @@
-// Lab 6 Assignment
-// Charles Haiwen & 
-// 26 April 2025
+/*
+   lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
 
-// initiatize variables
-var myTransport = ["Nissan skyline", "Santa Cruz Metro"];
+   Requirements: jQuery must be loaded for this script to work.
 
-//create object
-myMainRide = {
-  make: "Nissan",
-  model: "Skyline",
-  color: "red",
-  year: 1995,
-  age: function() {
-    return 2025 - year;
+   Lab 11 Assignment
+   Author: Charles Haiwen & Austin Allen
+   Date: 17 May 2025
+*/
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
+}
+
+// shuffle function
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
   }
 }
 
-//output
-document.writeln("Kinds of transportation I use: " + myTransport + "<br>");
-document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
+// click listener for button
+$("#submit").click(function(){
+  // get value of input field
+  const userName = $("#user-name").val();
+
+  // now let's sort it
+  userNameSorted = sortString(userName);
+
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
