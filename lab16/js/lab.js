7b1ede7235d4e811323089cf55c2e82fbd299058
Magmyte <charleshaiwen@gmail.com>
2025-06-02 12:39:12 -0700

    API Testing

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,20 +2,36 @@
 // Charles Haiwen & 
 // 26 April 2025
 
-// initiatize variables
-var myTransport = ["Nissan skyline", "Santa Cruz Metro"];
+// button event listener
+$("#activate").click(function () {
+  // Using the core $.ajax() method
+  $.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://cataas.com/cat",
+    // The data to send (will be converted to a query string)
+    data: {
+      // here is where any data required by the api 
+      //   goes (check the api docs)
+      // id: 123,
+      // api_key: "blahblahblah",
+    },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType: "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function (data) {
+      // do stuff
+      console.log(data);
+      var imgURL = data.url;
+      $("#output").append("<img src='" + imgURL + "'>");
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+      // do stuff
+      console.log("Error:", textStatus, errorThrown);
+    }
+  })
 
-//create object
-myMainRide = {
-  make: "Nissan",
-  model: "Skyline",
-  color: "red",
-  year: 1995,
-  age: function() {
-    return 2025 - year;
-  }
-}
-
-//output
-document.writeln("Kinds of transportation I use: " + myTransport + "<br>");
-document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
+});
