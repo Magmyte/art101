bcde39313067e0280c0731b6cbef3d3003d43d52
Magmyte <charleshaiwen@gmail.com>
2025-06-05 01:33:24 -0700

    Lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,9 +1,33 @@
-// Lab 6 Assignment
-// Charles Haiwen & 
-// 26 April 2025
+// Lab 16 Assignment
+// Charles Haiwen & Austin Allen
+// 5 June 2025
 
-// button event listener
-$("#activate").click(function () {
-  
-
-});
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://xkcd.com/json.html",
+    // The data to send (will be converted to a query string)
+    data: { 
+            // here is where any data required by the api 
+            //   goes (check the api docs)
+            // id: 123,
+            // api_key: "blahblahblah",
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        console.log(data);
+        var titleText = data.title;
+        $("#output").html("<h3>" + titleText + "</h3>");
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) { 
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+})
